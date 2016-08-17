0161eede77f6cd7bb89cf484248abb63ccd7a473
alex <yukhymenko.oleksandr@gmail.com>
2016-08-17 18:37:54 +0300

    testing

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -19,15 +19,13 @@ class BasicSimulation extends Simulation {
   val firstScn = scn.inject(
     /*rampUsers(8000) over (10 seconds),
     nothingFor(5 seconds),*/
-    constantUsersPerSec(200) during (20 seconds),
-    nothingFor(5 seconds),
-    constantUsersPerSec(400) during (20 seconds),
-    nothingFor(5 seconds)
-    /*constantUsersPerSec(6000) during (20 seconds),
-    nothingFor(5 seconds),
-    constantUsersPerSec(8000) during (20 seconds),
-    nothingFor(5 seconds),
-    constantUsersPerSec(10000) during (20 seconds)*/
+    constantUsersPerSec(2000) during (20 seconds),
+    nothingFor(10 seconds),
+    constantUsersPerSec(3000) during (20 seconds),
+    nothingFor(10 seconds),
+    constantUsersPerSec(4000) during (20 seconds),
+    nothingFor(10 seconds),
+    constantUsersPerSec(5000) during (20 seconds)
   ).protocols(httpConf)
 
   setUp(
