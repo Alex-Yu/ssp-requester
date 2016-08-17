aff7f0631b11b4d4fed19798c9f6aaa738be56e5
alex <yukhymenko.oleksandr@gmail.com>
2016-08-17 18:29:41 +0300

    testing

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -19,9 +19,9 @@ class BasicSimulation extends Simulation {
   val firstScn = scn.inject(
     /*rampUsers(8000) over (10 seconds),
     nothingFor(5 seconds),*/
-    constantUsersPerSec(2000) during (20 seconds),
+    constantUsersPerSec(200) during (20 seconds),
     nothingFor(5 seconds),
-    constantUsersPerSec(4000) during (20 seconds),
+    constantUsersPerSec(400) during (20 seconds),
     nothingFor(5 seconds)
     /*constantUsersPerSec(6000) during (20 seconds),
     nothingFor(5 seconds),
