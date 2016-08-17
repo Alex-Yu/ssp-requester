aa62ed4b880e985a3dbc37ebf1d2ace1d789b98f
alex <yukhymenko.oleksandr@gmail.com>
2016-08-17 13:55:46 +0300

    add limit for adRequester feed

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -6,15 +6,16 @@ import io.gatling.http.Predef._
 import scala.io.Source
 import scala.util.Random
 import Lib._
+import com.sun.org.apache.xalan.internal.utils.XMLSecurityManager.Limit
 
 class BasicSimulation extends Simulation {
 
   val httpConf = http
     .baseURL("http://127.0.0.1:8080")
-//    .baseURL("http://209.205.218.34:8080")
+    //    .baseURL("http://209.205.218.34:8080")
     .contentTypeHeader("application/json")
 
-  val feeder = getFeeder.circular
+  val feeder = getFeeder().random
 
   val scn = scenario("Load test")
     .feed(feeder)
@@ -34,13 +35,15 @@ object Lib {
 
   private val r = Random
 
-  private val adRequests = Source.fromFile("output.log").getLines()
+  private val adRequests = Source.fromFile("output_all.log").getLines()
 
   private val sources = Source.fromFile("sources.txt").getLines().filter(_.nonEmpty).toArray
   private val sourcesQtty = sources.length
 
-  def getFeeder = adRequests.map( s => Map("json" -> s, "sid" -> getRandomSource) ).toArray
+  def getFeeder(limit: Int = 10000) =
+    (1 to limit) map ( _ => Map("json" -> adRequests.next(), "sid" -> getRandomSource) ) toArray
 
   def getRandomSource = sources(r.nextInt(sourcesQtty))
 
 }
+
