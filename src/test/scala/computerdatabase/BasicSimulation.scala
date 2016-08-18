22974d5eb5cbfeeb2254dd42babe808297a342ec
alex <yukhymenko.oleksandr@gmail.com>
2016-08-18 12:56:59 +0300

    testing

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -16,7 +16,7 @@ class BasicSimulation extends Simulation {
     .maxConnectionsPerHost(300)
     .shareConnections
 
-  val rps = 9000
+  val rps = 8000
 
 //  val firstScn = scenario("load").exec(Array.fill(1000)(AdRequest.adRequest))
   val firstScn = scenario("load").exec(AdRequest.adRequest)
