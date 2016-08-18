406a8511bae7e9a8ce4f18b572960b7b8461d321
alex <yukhymenko.oleksandr@gmail.com>
2016-08-18 13:07:24 +0300

    testing

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -10,31 +10,36 @@ import scala.util.Random
 class BasicSimulation extends Simulation {
 
   val httpConf = http
-//    .baseURL("http://127.0.0.1:8080")
-        .baseURL("http://209.205.218.34:8080")
+    //    .baseURL("http://127.0.0.1:8080")
+    .baseURL("http://209.205.218.34:8080")
     .contentTypeHeader("application/json")
     .maxConnectionsPerHost(300)
     .shareConnections
 
-  val rps = 8000
+  val rps = 10000
+  val halfRps = rps / 2
+  val quartRps = rps / 4
 
-//  val firstScn = scenario("load").exec(Array.fill(1000)(AdRequest.adRequest))
+  //  val firstScn = scenario("load").exec(Array.fill(1000)(AdRequest.adRequest))
   val firstScn = scenario("load").exec(AdRequest.adRequest)
     .inject(
-    rampUsersPerSec(1) to rps during (10 seconds),
-    constantUsersPerSec(rps) during (50 seconds)/*,
-    nothingFor(10 seconds),
-    constantUsersPerSec(7000) during (60 seconds),
-    nothingFor(10 seconds),
-    constantUsersPerSec(10000) during (60 seconds)*/
-  ).protocols(httpConf)
+      rampUsersPerSec(1) to quartRps during (10 seconds),
+      constantUsersPerSec(quartRps) during (50 seconds),
+      nothingFor(10 seconds),
+      rampUsersPerSec(quartRps) to halfRps during (10 seconds),
+      constantUsersPerSec(halfRps) during (50 seconds),
+      nothingFor(10 seconds),
+      rampUsersPerSec(halfRps) to rps during (10 seconds),
+      constantUsersPerSec(rps) during (50 seconds),
+      nothingFor(10 seconds)
+    ).protocols(httpConf)
 
   setUp(
     firstScn
-  ).throttle(
+  )/*.throttle(
     reachRps(rps) in (10 seconds),
     holdFor(50 seconds)
-  )
+  )*/
 
 
 }
