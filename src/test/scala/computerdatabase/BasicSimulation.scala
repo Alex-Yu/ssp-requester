c98d60c45ac49f78d98533d716c74328d573ba25
alex <yukhymenko.oleksandr@gmail.com>
2016-08-23 13:18:20 +0300

    real test

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -13,31 +13,32 @@ class BasicSimulation extends Simulation {
   val httpConf = http
     //    .baseURL("http://127.0.0.1:8080")
 //    .baseURL("http://209.205.218.34:8080")
-    .baseURL("http://209.205.219.218:8080")
+//    .baseURL("http://209.205.219.218:8080")
+    .baseURL("http://209.205.219.186:80")
     .contentTypeHeader("application/json")
     .maxConnectionsPerHost(300)
     .shareConnections
 
-  val rps = 8000
+  val rps = 26000
   val extra = rps + 1000
   val halfRps = rps / 2
   val quartRps = rps / 4
+  val hqRps = halfRps + quartRps
 
   //  val firstScn = scenario("load").exec(Array.fill(1000)(AdRequest.adRequest))
   val firstScn = scenario("load").exec(AdRequest.adRequest)
     .inject(
       rampUsersPerSec(1) to quartRps during (10 seconds),
       constantUsersPerSec(quartRps) during (50 seconds),
-      nothingFor(10 seconds),
       rampUsersPerSec(quartRps) to halfRps during (10 seconds),
       constantUsersPerSec(halfRps) during (50 seconds),
-      nothingFor(10 seconds),
       rampUsersPerSec(halfRps) to rps during (10 seconds),
       constantUsersPerSec(rps) during (50 seconds),
-      nothingFor(10 seconds),
-      rampUsersPerSec(rps) to extra during (10 seconds),
+      rampUsersPerSec(halfRps) to hqRps during (10 seconds),
+      constantUsersPerSec(hqRps) during (50 seconds),
+      rampUsersPerSec(hqRps) to extra during (10 seconds),
       constantUsersPerSec(extra) during (20 seconds),
-      nothingFor(10 seconds)
+      nothingFor(5 seconds)
     ).protocols(httpConf)
 
   setUp(
