a74debd371da1b281241e1c7ec0c8dd506f0b821
alex <yukhymenko.oleksandr@gmail.com>
2016-08-25 13:42:46 +0300

    add random rtb / non-rtb

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -68,16 +68,16 @@ object Lib {
 
   def getFeeder(limit: Int = 1000) =
     (1 to limit).map { _ =>
-      if (isRtb)
+//      if (isRtb)
         Map(
           "json" -> adRequests.next(),
           "query" -> s"/bidder?sid=$getRandomSource"
         )
-      else
+ /*     else
         Map(
           "json" -> "",
           "query" -> s"/bidder?&sid=$getRandomSource&pubId=15&pubName=test&domain=google.com&ua=Mozilla/6.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_10_4)%20AppleWebKit/600.7.12%20(KHTML,%20like%20Gecko)%20Version/8.0.7%20Safari/600.7.12&ip=190.93.245.15&h=70&w=300&maxd=30&floor=0.1&os=Mac%20OS%20X%2010_10_4&aid=123&pid=12"
-        )
+        )*/
     }.toArray
 
   def getRandomSource = sources(r.nextInt(sourcesQtty))
