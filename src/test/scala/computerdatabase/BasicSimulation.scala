8e3633af4866798c0a51bf1d87cd7234b9c788d3
alex <yukhymenko.oleksandr@gmail.com>
2016-09-05 17:28:09 +0300

    some

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -11,19 +11,15 @@ import scala.util.Random
 class BasicSimulation extends Simulation {
 
   val httpConf = http
-//    .baseURL("http://localhost:8080")
-//        .baseURL("http://209.205.219.186:80")
-//        .baseURL("http://209.205.219.58:8080")
-        .baseURL("http://aux-log.videe.tv")
+    //    .baseURL("http://localhost:8080")
+    .baseURL("http://209.205.218.34:8090")
+    //        .baseURL("http://209.205.219.58:8080")
+    //        .baseURL("http://aux-log.videe.tv")
     .contentTypeHeader("application/json")
-    .maxConnectionsPerHost(600)
+    .maxConnectionsPerHost(15000)
     .shareConnections
 
-  val rps = 20000
-  val extra = rps * 1.1
-  val halfRps = rps / 2
-  val quartRps = rps / 4
-  val hqRps = halfRps + quartRps
+  val rps = 60000
 
   val firstScn = scenario("load").exec(AdRequest.adRequest)
     .inject(
@@ -60,9 +56,9 @@ object AdRequest {
         )
       } {
         exec(
-        http(s"Rtb")
-          .post("${query}")
-          .body(StringBody("${json}"))
+          http(s"Rtb")
+            .post("${query}")
+            .body(StringBody("${json}"))
         )
       }
 }
@@ -71,23 +67,17 @@ object Lib {
 
   private val r = Random
 
-  private val adRequests = Source.fromFile("output_all.log").getLines()
+
 
   private val sources = Source.fromFile("sources.txt").getLines().filter(_.nonEmpty).toArray
   private val sourcesQtty = sources.length
 
   def getFeeder(limit: Int = 1000) =
     (1 to limit).map { _ =>
-      if (isRtb(0))
-        Map(
-          "json" -> adRequests.next(),
-          "query" -> s"/bidder?sid=$getRandomSource"
-        )
-      else
-        Map(
-          "json" -> "",
-          "query" -> s"/bidder?&sid=$getRandomSource&pubId=15&pubName=test&domain=google.com&ua=Mozilla/6.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_10_4)%20AppleWebKit/600.7.12%20(KHTML,%20like%20Gecko)%20Version/8.0.7%20Safari/600.7.12&ip=$getRandomIp&h=70&w=300&maxd=30&floor=0.1&os=Mac%20OS%20X%2010_10_4&aid=123&pid=12&adid=100500"
-        )
+      Map(
+        "json" -> "",
+        "query" -> s"/bidresponse"
+      )
     }.toArray
 
   def getRandomSource = sources(r.nextInt(sourcesQtty))
